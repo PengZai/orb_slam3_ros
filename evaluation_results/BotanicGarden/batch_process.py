d0612646e4a533cd20cfce41805ac12c90ad30b4
PengZai <649365461@qq.com>
2025-02-20 14:39:00 +0000

    optimize the process of saving results

--- evaluation_results/BotanicGarden/batch_process.py
+++ evaluation_results/BotanicGarden/batch_process.py
@@ -2,43 +2,66 @@ import os
 import numpy as np 
 from evo.tools import file_interface
 from evo.core.trajectory import PoseTrajectory3D
+import shutil
 
 folder = "1018_00"
 filename = "orbslam3_stereo_inertial_without_loop_clourse_for_1018_00_img10hz600p_kf_traj.txt"
+
+if "cam" in filename.split("_"):
+    frame_type = "cam"
+else:
+    frame_type = "key_frame"
+
+
 filename.split(".")
 filename_suffix = '.' + filename.split(".")[1]
 filename = filename.split(".")[0]
 
-# # VLP16 in RGB0 coordinates
-transform_matrix = np.array([
-    [0.0238743541600432,-0.999707744440396,0.00360642510766516,0.138922870923538],  
-    [-0.00736968896588375,-0.00378431903190059,-0.999965147452649 ,-0.177101909101325],  
-    [0.999687515506770,0.0238486947027063,-0.00745791352160211,-0.126685267545513],  
-    [0.0,0.0,0.0,1.0]
-])
 
-# VLP16 in Xsens coordinates
-# transform_matrix = np.array([
-#         [0.999678872580465,0.0252865664429322,0.00150422292234868,0.0584867781527745],
-#         [-0.0252723438960774,0.999649431893338,-0.0078025434141585,0.00840419966766332],
-#         [-0.00170103929405540,0.00776298237926191,0.99996789371916,0.168915521980526],
-#         [0.0,0.0,0.0,1.0]
+# # VLP16 in RGB0 coordinates
+# sensor_coordinate_transform_matrix = np.array([
+#     [0.0238743541600432,-0.999707744440396,0.00360642510766516,0.138922870923538],  
+#     [-0.00736968896588375,-0.00378431903190059,-0.999965147452649 ,-0.177101909101325],  
+#     [0.999687515506770,0.0238486947027063,-0.00745791352160211,-0.126685267545513],  
+#     [0.0,0.0,0.0,1.0]
 # ])
 
-# transform_matrix = np.array([
+# keep same, it seems we don't need to transfer to VLP16 coordinates, which is more precise
+sensor_coordinate_transform_matrix = np.array([
+    [1.,0,0,0],  
+    [0,1.,0,0],  
+    [0,0,1.,0],  
+    [0,0,0,1.0]
+])
+
+
+# seems x in key frame of orbslam system is z in BotanicGarden, and y is -y
+system_coordinate_transform_matrix_for_key_frame = np.array([
+    [0.,  1.,  0.,   0],  
+    [-1., 0.,  0.,   0],  
+    [0.,  0., 1.,   0],  
+    [0.,  0,   0,   1.0]
+])
+
+# seems x in camera frame of orbslam system is z in BotanicGarden, and y is -y
+system_coordinate_transform_matrix_for_camera_frame = np.array([
+    [1., 0,  0.,    0],  
+    [0., 1., 0.,  0],  
+    [0.,  0., 1.,  0],  
+    [0,  0,  0,  1.0]
+])
+
+
+
+
+# keep same
+# system_coordinate_transform_matrix = np.array([
 #     [1.,0,0,0],  
 #     [0,1.,0,0],  
 #     [0,0,1.,0],  
 #     [0,0,0,1.0]
 # ])
 
-# seems x in orbslam system is z in BotanicGarden, and y is -y
-adjusted_matrix = np.array([
-    [0, 0,  1.,    0],  
-    [0., -1., 0.,  0],  
-    [1.,  0., 0.,  0],  
-    [0,  0,  0,  1.0]
-])
 
 
 
@@ -48,9 +71,13 @@ traj.timestamps = traj.timestamps / 1e9
 transformed_poses = []
 for pose in traj.poses_se3:
 
+    if frame_type == "cam":
     # Apply the transformation
-    transformed_pose_matrix =   transform_matrix @ adjusted_matrix @ pose
+        transformed_pose_matrix =   sensor_coordinate_transform_matrix @ system_coordinate_transform_matrix_for_camera_frame @ pose
     
+    else: 
+        transformed_pose_matrix =   sensor_coordinate_transform_matrix @ system_coordinate_transform_matrix_for_key_frame @ pose
+
     # Append transformed pose as SE3 object
     transformed_poses.append(transformed_pose_matrix)
 
@@ -60,4 +87,8 @@ traj = PoseTrajectory3D(
 )
 
 
-file_interface.write_tum_trajectory_file(os.path.join(folder, filename+"_insecond"+filename_suffix), traj)
+if not os.path.exists(os.path.join(folder, filename)):
+    os.makedirs(os.path.join(folder, filename))
+
+shutil.copy(os.path.join(folder, filename + filename_suffix), os.path.join(folder, filename, filename + filename_suffix))
+file_interface.write_tum_trajectory_file(os.path.join(folder, filename, filename+"_coordinate_aligned"+filename_suffix), traj)
--- evaluation_results/evo_eval.sh
+++ evaluation_results/evo_eval.sh
@@ -1,11 +1,11 @@
 DIR="BotanicGarden/1018_00"
 
-ESITMATED="orbslam3_stereo_inertial_without_loop_clourse_for_1018_00_img10hz600p_kf_traj_insecond.txt"
-REFERENCE="gt_poses.tum"
+ESITMATED_FOLDER="orbslam3_stereo_inertial_without_loop_clourse_for_1018_00_img10hz600p_kf_traj"
+REFERENCE="1018_00_GT_output.txt"
 
-evo_rpe tum $DIR/$ESITMATED $DIR/$REFERENCE  > $DIR/rpe_$ESITMATED.log
-evo_ape tum $DIR/$ESITMATED $DIR/$REFERENCE  > $DIR/ape_$ESITMATED.log
-evo_ape tum $DIR/$ESITMATED $DIR/$REFERENCE --align --pose_relation trans_part > $DIR/ate_$ESITMATED.log
+evo_rpe tum $DIR/${ESITMATED_FOLDER}/${ESITMATED_FOLDER}_coordinate_aligned.txt $DIR/$REFERENCE  > $DIR/${ESITMATED_FOLDER}/rpe_${ESITMATED_FOLDER}_coordinate_aligned.log
+evo_ape tum $DIR/${ESITMATED_FOLDER}/${ESITMATED_FOLDER}_coordinate_aligned.txt $DIR/$REFERENCE  > $DIR/${ESITMATED_FOLDER}/ape_${ESITMATED_FOLDER}_coordinate_aligned.log
+evo_ape tum $DIR/${ESITMATED_FOLDER}/${ESITMATED_FOLDER}_coordinate_aligned.txt $DIR/$REFERENCE --align --pose_relation trans_part > $DIR/${ESITMATED_FOLDER}/ate_${ESITMATED_FOLDER}_coordinate_aligned.log
 
 
-evo_traj tum $DIR/$ESITMATED --ref=$DIR/$REFERENCE -p --plot_mode=xy --save_plot $DIR/ 
+evo_traj tum $DIR/${ESITMATED_FOLDER}/${ESITMATED_FOLDER}_coordinate_aligned.txt --ref=$DIR/$REFERENCE -p --plot_mode=xyz --save_plot $DIR/${ESITMATED_FOLDER}/ 
