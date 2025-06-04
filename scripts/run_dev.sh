1029c4e8c3aae4249f0a6528a13f38cd32e3b176
PengZai <649365461@qq.com>
2025-06-04 17:24:24 +0100

    add configure for PolyTunnel

--- scripts/run_dev.sh
+++ scripts/run_dev.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
+PROJECT_NAME="orb-slam3"
 
 IMAGE_NAME="orb_slam3:noetic-ros"
 DATA_PATH="/media/${USER}/zhipeng_usb/datasets"
+DATA_PATH2="/media/${USER}/zhipeng_8t/datasets"
 # Pick up config image key if specified
 if [[ ! -z "${CONFIG_DATA_PATH}" ]]; then
     DATA_PATH=$CONFIG_DATA_PATH
 fi
-
-
-PROJECT_DIR=$(pwd)
-PROJECT_NAME=$(basename "$PWD")
 
 
 docker build -t $IMAGE_NAME -f "${HOME}/vscode_projects/orb_slam3_ros/catkin_ws/src/orb_slam3_ros/Dockerfile_for_dev.txt" .
@@ -25,6 +23,7 @@ docker run \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
     -v ${HOME}/vscode_projects/orb_slam3_ros/catkin_ws:/root/catkin_ws \
     -v ${DATA_PATH}:/root/datasets \
+    -v ${DATA_PATH2}:/root/datasets2 \
     --privileged \
     --cap-add sys_ptrace \
     --runtime=nvidia \
